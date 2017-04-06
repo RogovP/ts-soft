fac4fae1760b0ed223d58243bf08f49a433b5c6b
RogovP <oooy_ee@mail.ru>
2017-04-06 15:10:49 +0300

    use reg expressions

--- functions/3.js
+++ functions/3.js
@@ -1,7 +1,8 @@
 function createPhoneNumber(masNum) {
   var str = masNum.join("");
   
-  return str.replace( /([\d]{3})([\d]{3})([\d]{4})/,//делим строку на нужные "элементы",чтобы использовать в дальнейшем
+  return str.replace( /([\d]{3})([\d]{3})([\d]{4})/,
+                     //делим строку на нужные "элементы",чтобы использовать в дальнейшем
                       "($1)$2-$3" );
 }
 console.log( createPhoneNumber( [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] ) );

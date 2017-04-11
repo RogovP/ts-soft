94bbc119ead7471e1aa039f5930e4b800b322e3d
RogovP <oooy_ee@mail.ru>
2017-04-11 10:10:43 +0300

    Update 7.js

--- functions/7.js
+++ functions/7.js
@@ -20,13 +20,10 @@ function reliableMultiply(a, b) {
     try {
       
       return  primitiveMultiply(a, b);
-
     } catch(e) {
       
       if (!(e instanceof MultiplicatorUnitFailure)){
-        
         throw e;
-        
       }
       
     }

2f41638199ca0654c3f86db3ec8957bb8ddadd79
RogovP <oooy_ee@mail.ru>
2017-04-11 10:15:29 +0300

    update code style

--- functions/9.js
+++ functions/9.js
@@ -5,12 +5,21 @@ function scoreTest(mas, bTrue, bNull, bFalse) {
     if (mas[i] === 0) {
       sum += bTrue;
     }
-      else if (mas[i] === 1) {
-            if (Math.sign (bNull) === -1){
-              sum += bNull;
-            }
-              else sum += bNull;
-          } else sum -= bFalse;
+    
+    else if (mas[i] === 1 ) {
+      
+      if (Math.sign (bNull) === -1) {
+        sum += bNull;
+      } else {
+        sum += bNull;
+      }
+      
+    }
+    
+    if(mas[i] === 2) {
+      sum -= bFalse;
+    }
+    
   }
   
   return sum;
